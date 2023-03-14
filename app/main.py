f304b69c7e62fe49f0c10cf6d5cd69a6ed727629
V-Perotto <vittorio.perotto@outlook.com>
2023-03-14 00:10:57 -0300

    Adicionando import da library 'Path' e 'os'; adicionando novo caminho para o template; corrigindo argumento do render_template.

--- app/main.py
+++ app/main.py
@@ -1,18 +1,25 @@
 from flask import Flask, render_template
+import os
+from pathlib import Path
 
-app = Flask(__name__)
+ROOT = Path(os.path.dirname(os.path.abspath(__file__))).parent
+CURDIR = os.getcwd()
+TEMPLATE_DIR = os.path.join(CURDIR, "templates")
+TEMPLATE_HTML_DIR = os.path.join(TEMPLATE_DIR, "html")
 
-## __name__ is the application name
+app = Flask(__name__, template_folder=TEMPLATE_HTML_DIR)
+
+# web
 @app.route('/')
 def index():
-    return render_template('html/home.html')
+    return render_template("home.html")
 
 @app.route('/orders')
 def orders():
     pedidos = ["Combo 1, comanda 2","Executivo 2, comanda 3",
                 "Refri laranja, comanda 134","Cerveja, comanda 12",
                 "Batata Frita, comanda 14"]
-    return render_template("html/orders.html", orders = pedidos)
+    return render_template("orders.html", orders = pedidos)
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port="05", debug=True)

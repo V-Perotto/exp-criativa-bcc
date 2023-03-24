1fa7ce8b61f118a1ad48ad252cde6e1c0bd35f53
V-Perotto <vittorio.perotto@outlook.com>
2023-03-23 23:25:34 -0300

    Corrigindo rotas e declarando funções

--- app/main.py
+++ app/main.py
@@ -1,26 +1,38 @@
-from flask import Flask, render_template
-import os
-from pathlib import Path
+from flask import Flask, redirect, render_template, request, url_for
 
-ROOT = Path(os.path.dirname(os.path.abspath(__file__))).parent
-CURDIR = os.getcwd()
-TEMPLATE_DIR = os.path.join(CURDIR, "templates")
-TEMPLATE_HTML_DIR = os.path.join(TEMPLATE_DIR, "html")
+app = Flask(__name__, template_folder="./templates/html/")
 
-app = Flask(__name__, template_folder=TEMPLATE_HTML_DIR)
+registered = []
 
-# web
 @app.route('/')
 def index():
     return render_template("home.html")
 
 @app.route('/orders')
 def orders():
-    pedidos = ["Combo 1, comanda 2","Executivo 2, comanda 3",
-                "Refri laranja, comanda 134","Cerveja, comanda 12",
-                "Batata Frita, comanda 14"]
-    return render_template("orders.html", orders = pedidos)
+    global registered
+    return render_template("orders.html", orders = registered)
+
+@app.route('/register_order', methods = ['POST', 'GET'])
+def register_order():
+    return render_template("register_order.html")
+
+@app.route('/save_order', methods = ['POST', 'GET'])
+def save_order():
+    if request.method == 'POST':
+        name = request.form['name']
+        ticket = request.form['ticket']
+    else:
+        name = request.form.get('name', None)
+        ticket = request.form.get('ticket', None)
+    
+    global registered
+    registered.append(f"{name}, comanda {ticket}")
+    # id = len(registered)+1
+    # registered[id] = f"{name}, comanda {ticket}"
+    
+    return redirect(url_for("orders"))
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port="05", debug=True)
+    app.run(host='127.0.0.1', port="5000", debug=True)
 

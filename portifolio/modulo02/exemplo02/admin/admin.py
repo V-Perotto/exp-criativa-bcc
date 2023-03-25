5d4658af2270941dd47ed5cd5875169fb1c84b0a
V-Perotto <vittorio.perotto@outlook.com>
2023-03-25 20:09:38 -0300

    Reorganizando e recriando página de Portfólio.

--- portifolio/modulo02/exemplo02/admin/admin.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-
-admin = Blueprint("admin", __name__, 
-                  static_folder="static", 
-                  template_folder="templates")
-
-@admin.route("/")
-def admin_index():
-    return "Esse é meu módulo de Admin"

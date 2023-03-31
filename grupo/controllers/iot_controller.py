e633bdbe7bb0f86f492ffcf573cc0fb6027219d6
V-Perotto <vittorio.perotto@outlook.com>
2023-03-31 16:55:30 -0300

    Adicionando rotas para IoT

--- grupo/controllers/iot_controller.py
+++ grupo/controllers/iot_controller.py
@@ -5,9 +5,24 @@ iot = Blueprint("iot", __name__, template_folder='./views/', static_folder='./st
 
 registered_iots = []
 
-hrefs = ["/iot/iot"]
-descriptions = ["Realizar iot"]
+hrefs = ["/iot/register"]
+descriptions = ["Registrar Sensor"]
 
 @iot.route('/')
 def iot_index():
     return render_template('/iot/index.html', hrefs=hrefs, descriptions=descriptions)
+
+@iot.route('/save_board', methods=['POST', 'GET'])
+def save_register():
+    global db_board
+
+    board_sensor_name = request.form.get("board_sensor_name", None)
+    board_sensor_status = request.form.get("board_sensor_status", None)
+    
+    dict_board = {
+        "name": board_sensor_name,
+        "status": board_sensor_status
+    }
+
+    db_board.append(dict_board)
+    return redirect(url_for("board.register"))

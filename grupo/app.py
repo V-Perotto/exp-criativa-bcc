5997779f5ad30f4b16df8854736cc28ca2d3ccbf
V-Perotto <vittorio.perotto@outlook.com>
2023-04-07 20:54:53 -0300

    Adicionando Blueprint de admin e iot; removendo import initial

--- grupo/app.py
+++ grupo/app.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template, session, g, url_for
+
 from controllers.admin_controller import admin
-from controllers.initial_controller import initial
 from controllers.iot_controller import iot
 from controllers.user_controller import user
 
 app = Flask(__name__, template_folder="./views/", static_folder="./static/")
 
+app.register_blueprint(admin, url_prefix='/admin')
 app.register_blueprint(user, url_prefix='/user')
+app.register_blueprint(iot, url_prefix='/IoT')
 
 @app.route('/')
 def index():

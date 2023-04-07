b75cbb4ae926ff80f2112768d881e0b9f62fb110
V-Perotto <vittorio.perotto@outlook.com>
2023-04-07 20:56:29 -0300

    Criando Blueprint de Iot

--- grupo/controllers/iot_controller.py
+++ grupo/controllers/iot_controller.py
@@ -1,15 +1,15 @@
 # TO DO
 from flask import Blueprint, render_template, redirect, url_for, request
 
-iot = Blueprint("iot", __name__, template_folder='./views/', static_folder='./static/', root_path="./")
+iot = Blueprint("IoT", __name__, template_folder='./views/', static_folder='./static/', root_path="./")
 
 registered_iots = []
 
-hrefs = ["/iot/register"]
+hrefs = ["/IoT/register"]
 descriptions = ["Registrar Sensor"]
 
 @iot.route('/')
-def iot_index():
+def index():
     return render_template('/iot/index.html', hrefs=hrefs, descriptions=descriptions)
 
 @iot.route('/save_board', methods=['POST', 'GET'])

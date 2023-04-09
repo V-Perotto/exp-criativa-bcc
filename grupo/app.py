4e05ee8202bd280b1b37028a341b55fbf1099a75
V-Perotto <vittorio.perotto@outlook.com>
2023-04-09 00:08:05 -0300

    Adicionando argumento de usuário; adicionando errorhandler para erro 404

--- grupo/app.py
+++ grupo/app.py
@@ -4,6 +4,8 @@ from controllers.admin_controller import admin
 from controllers.iot_controller import iot
 from controllers.user_controller import user
 
+from models.auth import getUser
+
 app = Flask(__name__, template_folder="./views/", static_folder="./static/")
 
 app.register_blueprint(admin, url_prefix='/admin')
@@ -12,7 +14,11 @@ app.register_blueprint(iot, url_prefix='/IoT')
 
 @app.route('/')
 def index():
-    return render_template("base.html")
+    return render_template("base.html", user=getUser())
+
+@app.errorhandler(404)
+def error_404(error):
+    return render_template("error/404/404.html", user=getUser()), 404
 
 if __name__ == "__main__":
     app.run(debug=True)

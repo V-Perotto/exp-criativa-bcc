9861e29db4b803156ac2a89ad7c75514937781e1
V-Perotto <vittorio.perotto@outlook.com>
2023-04-10 14:14:17 -0300

    Editando rotas existentes e criando novas rotas para listagem e criação de usuários

--- grupo/controllers/admin_controller.py
+++ grupo/controllers/admin_controller.py
@@ -1,17 +1,34 @@
 # TO DO
 from flask import Blueprint, render_template, redirect, url_for, request
-from models.auth import getUser
+from models.auth import getUser, registerUser
 
 admin = Blueprint("admin", __name__, template_folder='./views/', static_folder='./static/', root_path="./")
 
-registered_admins = []
+# tela index de admin?
 
-hrefs = ["/admin/admin"]
-descriptions = ["Realizar admin"]
+@admin.route('/Users')
+def users_index():
+    return render_template('/admin/Users/index.html', 
+                           user=getUser())
 
-@admin.route('/')
-def admin_index():
-    return render_template('/admin/index.html', 
-                           user=getUser(), 
-                           hrefs=hrefs, 
-                           descriptions=descriptions)
+@admin.route("/Users/Register")
+def register_users():
+    return render_template('/admin/Users/register_user.html', 
+                           user=getUser())
+
+@admin.route("/Users/UsersList")
+def user_list():
+    return render_template("/admin/Users/list_of_users.html",
+                           user=getUser())
+
+@admin.route('/Users/create_user', methods=['POST', 'GET']) 
+def create_user():
+    
+    user_name = request.form.get("user_name", None)
+    user_surname = request.form.get("user_surname", None)
+    user_email = request.form.get("user_email", None)
+    user_password = request.form.get("user_password", None)
+    user_is_admin = request.form.get("user_is_admin", None)
+    
+    registerUser(user_name, user_surname, user_email, user_password, user_is_admin)
+    return redirect(url_for("admin.create_user"))

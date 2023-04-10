3a5ba27598ce82e5768780a84c190ea2fe64cf6c
V-Perotto <vittorio.perotto@outlook.com>
2023-04-10 14:20:58 -0300

    Removendo variáveis e função de teste; realizando novos imports e renomeando variáveis existentes

--- grupo/controllers/user_controller.py
+++ grupo/controllers/user_controller.py
@@ -1,47 +1,31 @@
 from flask import Blueprint, render_template, redirect, url_for, request
-from models.auth import getUser
+from models.auth import getUser, registerUser, validate_login
+from models.auth import db_users
 
 user = Blueprint("user", __name__, template_folder='./views/', static_folder='./static/', root_path="./")
-
-db_users = []
-
-hrefs = ["/user/register", "/user/login"]
-descriptions = ["Criar um Cadastro", "Realizar Login"]
 
 @user.route('/')
 def user_index():
     return render_template('/user/index.html', 
-                           user=getUser(), 
-                           hrefs=hrefs, 
-                           descriptions=descriptions)
+                           user=getUser()
+                           )
 
 # register user
 @user.route('/register')
 def register():
-    global db_users
     return render_template('/user/register.html', 
                            user=getUser(), 
-                           hrefs=hrefs, 
-                           descriptions=descriptions, 
-                           register_list = db_users)
+                           db_users = db_users)
 
 @user.route('/save_register', methods=['POST', 'GET'])
 def save_register():
-    global db_users
-
     user_name = request.form.get("user_name", None)
     user_surname = request.form.get("user_surname", None)
     user_email = request.form.get("user_email", None)
-    user_pwd = request.form.get("user_pwd", None)
+    user_password = request.form.get("user_password", None)
     # user_birthday = request.form.get("user_birthday", None)
     
-    dict_user = {
-        "user_name": user_name,
-        "user_surname": user_surname,
-        "user_email": user_email,
-        "user_pwd": user_pwd
-    }
-    db_users.append(dict_user)
+    registerUser(user_name, user_surname, user_email, user_password)
     return redirect(url_for("user.register"))
 
 # login
@@ -50,26 +34,10 @@ def user_login():
     global db_users 
     
     user_email = request.form.get("user_email", None)
-    user_pwd = request.form.get("user_pwd", None)
+    user_pwd = request.form.get("user_password", None)
     
     is_valid = validate_login(user_email, user_pwd)
     
     return render_template('/user/login.html', 
                            user=getUser(), 
-                           hrefs=hrefs, 
-                           descriptions=descriptions, 
                            user_registered = is_valid)
-
-def validate_login(user_email, user_pwd):
-    global db_users
-    for user in db_users:
-        if user['user_email'] == user_email and user["user_pwd"] == user_pwd:
-            return True
-    return False 
-# @user.route('/save_login', methods=['POST', 'GET'])
-# def save_login():
-#     global db_users 
-
-#     user_email = request.form.get("user_email", None)
-#     user_pwd = request.form.get("user_pwd", None)
-#     return render_template('/user/login.html', hrefs=hrefs, descriptions=descriptions, registered_users = db_users)

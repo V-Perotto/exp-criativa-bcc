bf042c6a8fab7e60f27575301402b76903a234b0
V-Perotto <vittorio.perotto@outlook.com>
2023-04-13 19:51:36 -0300

    Adicionando novas rotas para listagem e criação de placas e sensores

--- grupo/controllers/iot_controller.py
+++ grupo/controllers/iot_controller.py
@@ -1,6 +1,6 @@
 # TO DO
 from flask import Blueprint, render_template, redirect, url_for, request
-from models.auth import getUser, list_board_sensor
+from models.auth import getUser, showBoards, showSensors
 
 iot = Blueprint("IoT", __name__, template_folder='./views/', static_folder='./static/', root_path="./")
 
@@ -8,20 +8,47 @@ iot = Blueprint("IoT", __name__, template_folder='./views/', static_folder='./st
 def index():
     return render_template('/iot/index.html', 
                            user=getUser(), 
-                           iot_list_board=list_board_sensor()
+                           )
+@iot.route('/SensorsList')
+def list_sensors():
+    return render_template('iot/list/index_sensors.html',
+                           user=getUser(), 
+                           db_board=showSensors()
                            )
 
-@iot.route('/save_board', methods=['POST', 'GET'])
-def save_register():
+@iot.route('/BoardsList')
+def list_boards():
+    return render_template('iot/list/index_boards.html',
+                           user=getUser(), 
+                           db_board=showBoards()
+                           )
+
+@iot.route('/add_board', methods=['POST', 'GET'])
+def add_board():
     global db_board
 
-    board_sensor_name = request.form.get("board_sensor_name", None)
-    board_sensor_status = request.form.get("board_sensor_status", None)
+    name = request.form.get("name", None)
+    status = request.form.get("status", None)
     
     dict_board = {
-        "name": board_sensor_name,
-        "status": board_sensor_status
+        "name": name,
+        "status": status
     }
 
     db_board.append(dict_board)
-    return redirect(url_for("board.register"))
+    return redirect(url_for("board.add_board"))
+
+@iot.route('/add_sensor', methods=['POST', 'GET'])
+def add_sensor():
+    global db_board
+
+    name = request.form.get("name", None)
+    status = request.form.get("status", None)
+    
+    dict_board = {
+        "name": name,
+        "status": status
+    }
+
+    db_board.append(dict_board)
+    return redirect(url_for("board.add_sensor"))

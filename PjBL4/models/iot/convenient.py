a177b0194cc04b1fc067c35469e02cb914223c63
V-Perotto <vittorio.perotto@outlook.com>
2023-06-16 09:13:28 -0300

    Adicionando função de update

--- PjBL4/models/iot/convenient.py
+++ PjBL4/models/iot/convenient.py
@@ -10,3 +10,12 @@ class Convenient(db.Model):
                                  Device.convenient, Device.description,  Device.is_active, Device.convenient).all()
         
         return convenients
+    
+    def update_convenient(data):
+        Device.query.filter_by(id=data['id'])\
+                .update(dict(name = data['name'], brand=data['brand'], model = data['model'], 
+                        voltage = data['voltage'], description = data['description'], 
+                        is_active = data['is_active']))
+        
+        Sensor.query.filter_by(id=data['id']).update(dict(is_active = data['is_active']))
+        db.session.commit()
